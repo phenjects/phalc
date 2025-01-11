cc127cad258daa0c93e37fba0667a9eaf6593d05
gorilla <yusukekitagawa7640@gmail.com>
2025-01-11 21:54:26 +0700

    fuck you

--- main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const rowOneCreate = document.createElement("button");
-    const rowTwoCreate = document.createElement("button");
-    const rowThreeCreate = document.createElement("button");
-})
-
-for (let i = 0; i <= 2; i++) {
-    
-}

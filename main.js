3483a39bd9b5e6c3f79b5ef6cb46ccd17814985b
gorilla <yusukekitagawa7640@gmail.com>
2025-01-12 03:16:42 +0700

    fix latr

---
+++ main.js
@@ -0,0 +1,14 @@
+const numbers = [
+    { one: document.getElementById("1") },
+    { two: document.getElementById("2") },
+    { three: document.getElementById("3") },
+    { four: document.getElementById("4") },
+    { five: document.getElementById("5") },
+    { six: document.getElementById("6") },
+    { seven: document.getElementById("7") },
+    { eight: document.getElementById("8") },
+    { nine: document.getElementById("9") },
+    { zero: document.getElementById("0") }
+];
+
+console.log(numbers);

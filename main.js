2920f4bf44fcab576cc46b25674ca6e23287d6f2
gorilla <yusukekitagawa7640@gmail.com>
2025-01-12 22:49:36 +0700

    die

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 const numberButtons = [
-document.getElementById("1") ,
-document.getElementById("2") ,
-document.getElementById("3") ,
-document.getElementById("4") ,
-document.getElementById("5") ,
-document.getElementById("6") ,
-document.getElementById("7") ,
-document.getElementById("8") ,
-document.getElementById("9") ,
-document.getElementById("0")
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+  document.getElementById("6"),
+  document.getElementById("7"),
+  document.getElementById("8"),
+  document.getElementById("9"),
+  document.getElementById("0"),
 ];
 
 const numberValues = "1234567890";
 
 console.log(numberButtons[1]);
 
-numberButtons.onclick = function() {
-    console.log("penis")
-}
+numberButtons.forEach((numberBtn) => {
+  numberBtn.onclick = function () {
+    console.log("penis");
+  };
+});

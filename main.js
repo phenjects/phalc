94ae74ff979cca7f9cf8e33bde46a505c1b59baa
gorilla <yusukekitagawa7640@gmail.com>
2025-01-14 03:02:59 +0700

    I'M FINALLY FUCKING FINISHED
    full phalculator functionality, css upgrade tmr. I'M FREEEE
    
    list:
    
    1. mathematical operator/sign buttons
    2. clear button
    3. equal operator/sign button
    
    endLine()

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// INFO: the big bad boy (just a simple array to get all the main phalculator buttons.)
 const numberButtons = [
   document.getElementById("1"),
   document.getElementById("2"),
@@ -9,14 +10,40 @@ const numberButtons = [
   document.getElementById("8"),
   document.getElementById("9"),
   document.getElementById("0"),
+  document.getElementById("+"),
+  document.getElementById("-"),
+  document.getElementById("*"),
+  document.getElementById("/"),
 ];
 
-const numberValues = "1234567890";
+// INFO: variables for buttons too small for an array and too special to be apart of the big bad boy
+const equalButton = document.getElementById("=");
+const clearButton = document.getElementById("C");
 
-console.log(numberButtons[1]);
+// INFO: gets the HTML input tag so the calc is visual and not in the console only
+const phalcResult = document.getElementById("resultID");
 
+// INFO: number value variables to calculate user number input
+let numberValues = "";
+let numberOutput;
+
+// INFO: makes each button a property of numberBtn
+// INFO: gets the inner HTML values of each button for the phalculator output
 numberButtons.forEach((numberBtn) => {
   numberBtn.onclick = function () {
-    window.prompt(numberBtn);
+    numberValues = numberValues.concat(numberBtn.innerHTML);
+    phalcResult.value = numberValues;
   };
+});
+
+// INFO: equal sign to output the result of the user input
+equalButton.addEventListener("click", function () {
+  numberValues = "";
+  phalcResult.value = eval(phalcResult.value);
+});
+
+// INFO: clears the user input incase they made a mistake
+clearButton.addEventListener("click", function () {
+  numberValues = "";
+  phalcResult.value = "";
 });
